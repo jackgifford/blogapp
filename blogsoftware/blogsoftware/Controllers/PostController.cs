a336ad2c39bdf4654dade204442ba01bc7169d1e
DESKTOP-TT8R5GP\jackg <jack.gifford2264@hotmail.com>
2017-01-30 23:57:00 +1000

    Post Controller handles indexing, and opening a specified post

--- blogsoftware/blogsoftware/Controllers/PostController.cs
+++ blogsoftware/blogsoftware/Controllers/PostController.cs
@@ -4,24 +4,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using blogsoftware.Attributes;
+using blogsoftware.Models;
 
 namespace blogsoftware.Controllers
 {
+    [SessionAuth]
     public class PostController : Controller
     {
-        AppContext db = new AppContext();
+        private readonly AppContext _db;
+
+        public PostController(AppContext db)
+        {
+            _db = db;
+        }
+
         // GET: Post
         public ActionResult Index()
         {
-            return View(db.Users.First(x => x.Username == "Admin").Posts.ToList());
+            //TODO: Integrate with redis
+            using (_db)
+            {
+                var user = _db.Users.First(x => x.Username == Session["Username"] as string);
+                return user.Posts.Any() ? View("Index", user.Posts) : View("Empty");
+            }
         }
 
-        public string Item(Guid itemIndex)
+        public ActionResult Item(Guid itemIndex)
         {
+            using (_db)
+            {
+                var post =
+                    _db.Users.First(x => x.Username == Session["Username"] as string)
+                        .Posts.FirstOrDefault(x => x.PostID == itemIndex);
 
-            return itemIndex.ToString();
+                return post != null ? View(post) : View("Error");
+            }
         }
 
+        [HttpGet]
+        public ActionResult New()
+        {
+            throw new NotImplementedException();
+        }
 
+        [HttpPost]
+        public bool SaveToDatabase()
+        {
+            //If true redirect to item? Or do it as json so we dont need a page refresh?
+            throw new NotImplementedException();
+        }
     }
 }
