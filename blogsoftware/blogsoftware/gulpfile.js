48dd24061613cdcf545791eacc95a3160716d4ac
DESKTOP-TT8R5GP\jackg <jack.gifford2264@hotmail.com>
2017-01-30 23:56:14 +1000

    Added gulp, and a gulp file

---
+++ blogsoftware/blogsoftware/gulpfile.js
@@ -0,0 +1,33 @@
+﻿var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var del = require('del');
+var sass = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+
+var paths = {
+    sass: "Content/scss/",
+    output: "Content/css/"
+};
+
+
+gulp.task("sass:compile", function () {
+    return gulp.src(paths.sass + "main.scss")
+        .pipe(sass())
+        .pipe(gulp.dest(paths.sass));
+});
+
+gulp.task("bundle:css", ["sass:compile"], function () {
+    return gulp.src(paths.sass + "main.css")
+		.pipe(concat("styles.css"))
+		.pipe(gulp.dest(paths.output))
+		.pipe(rename({ suffix: ".min" }))
+		.pipe(cssmin())
+		.pipe(gulp.dest(paths.output));
+});
+
+gulp.task("watch", function () {
+    gulp.watch(paths.sass + "**/*.scss", ["bundle:css"]);
+});
+
